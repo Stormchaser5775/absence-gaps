b058bc3c25ed265e4491cf1e22931dc436272ee3
Stormchaser5775 <144896133+Stormchaser5775@users.noreply.github.com>
2025-10-17 19:41:43 +0530

    added to script file

--- run_experiment.py
+++ run_experiment.py
@@ -1,11 +1,22 @@
 import os
 import json
 from together import Together # type: ignore
+from transformers import AutoModelForCausalLM, AutoTokenizer # type: ignore
+import torch
+
+# Load tokenizer and model
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.1-8B-Instruct")
+model = AutoModelForCausalLM.from_pretrained(
+    "meta-llama/Llama-3.1-8B-Instruct",
+    torch_dtype=torch.float16,
+    device_map="auto"
+)
 
 data = []
 with open("data/toy.jsonl", 'r') as f:
      for line in f:
           data.append(json.loads(line))
 
-# for i in data:
-#      data[i][""]
+
+for i in range(len(data)):
+     data[i]["question"]
