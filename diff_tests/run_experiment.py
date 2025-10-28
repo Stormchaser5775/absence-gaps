e297b7276048c274f20bff3ee2b51657f0e48589
Stormchaser5775 <144896133+Stormchaser5775@users.noreply.github.com>
2025-10-28 17:26:50 +0530

    reördered prompts

--- diff_tests/run_experiment.py
+++ diff_tests/run_experiment.py
@@ -33,43 +33,43 @@ for n, line in enumerate(data):
 
      userPrompt1 = "Here is the complete original diff: " + oData + "\nAnd here is the merge diff after the developer fixed the commit history: " + cData + "\nWhat changed lines (insertions or deletions) present in the original diff are missing in the merge diff (if any)? List only the missing changed lines as a python list with no slash-n new lines and each line in double quotes; nothing else."
 
-     # userPrompt2 = "Here is the complete original diff: " + oData + "\nAnd here is the merge diff after the developer fixed the commit history: " + cData + "\nWhat changed lines (insertions or deletions) present in the original diff are missing in the merge diff (if any)? To answer this, compare line by line and find the lines without matching pairs. List only the missing changed lines as a python list with no slash-n new lines and each line in double quotes; nothing else. Search the whole document."
+     userPrompt2 = "Here is the complete original diff: " + oData + "\nAnd here is the merge diff after the developer fixed the commit history: " + cData + "\nWhat changed lines (insertions or deletions) present in the original diff are missing in the merge diff (if any)? List only the missing changed lines as a python list with no slash-n new lines and each line in double quotes; nothing else. Search the whole document."
+     
+     userPrompt3 = "Here is the complete original diff: " + oData + "\nAnd here is the merge diff after the developer fixed the commit history: " + cData + "\nWhat changed lines (insertions or deletions) present in the original diff are missing in the merge diff (if any)? To answer this, compare line by line and find the lines without matching pairs. List only the missing changed lines as a python list with no slash-n new lines and each line in double quotes; nothing else."
 
-     # userPrompt3 = "Here is the complete original diff: " + oData + "\nAnd here is the merge diff after the developer fixed the commit history: " + cData + "\nWhat changed lines (insertions or deletions) present in the original diff are missing in the merge diff (if any)? To answer this, divide each document into lists one new lines, compare line by line, and find the lines without matching pairs. Double check that the missing lines actually exist in the original document. There are only 5 missing lines. List only the missing changed lines as a python list with no slash-n new lines and each line in double quotes; nothing else. Search the whole document."
-
-     userPrompt4 = "Here is the complete original diff: " + oData + "\nAnd here is the merge diff after the developer fixed the commit history: " + cData + "\nWhat changed lines (insertions or deletions) present in the original diff are missing in the merge diff (if any)? List only the missing changed lines as a python list with no slash-n new lines and each line in double quotes; nothing else. Search the whole document."
+     # userPrompt4 = "Here is the complete original diff: " + oData + "\nAnd here is the merge diff after the developer fixed the commit history: " + cData + "\nWhat changed lines (insertions or deletions) present in the original diff are missing in the merge diff (if any)? To answer this, divide each document into lists one new lines, compare line by line, and find the lines without matching pairs. Double check that the missing lines actually exist in the original document. There are only 5 missing lines. List only the missing changed lines as a python list with no slash-n new lines and each line in double quotes; nothing else. Search the whole document."
 
      response1 = client.chat.completions.create(
           model="meta-llama/Llama-3-70b-chat-hf",
           messages=[{"role": "system", "content": systemPrompt}, {"role": "user", "content": userPrompt1}]
      )
 
-     # response2 = client.chat.completions.create(
-     #      model="meta-llama/Llama-3-70b-chat-hf",
-     #      messages=[{"role": "system", "content": systemPrompt}, {"role": "user", "content": userPrompt2}]
-     # )
-
-     # response3 = client.chat.completions.create(
-     #      model="meta-llama/Llama-3-70b-chat-hf",
-     #      messages=[{"role": "system", "content": systemPrompt}, {"role": "user", "content": userPrompt3}]
-     # )
-
-     response4 = client.chat.completions.create(
+     response2 = client.chat.completions.create(
           model="meta-llama/Llama-3-70b-chat-hf",
-          messages=[{"role": "system", "content": systemPrompt}, {"role": "user", "content": userPrompt4}]
+          messages=[{"role": "system", "content": systemPrompt}, {"role": "user", "content": userPrompt2}]
      )
 
+     response3 = client.chat.completions.create(
+          model="meta-llama/Llama-3-70b-chat-hf",
+          messages=[{"role": "system", "content": systemPrompt}, {"role": "user", "content": userPrompt3}]
+     )
+
+     # response4 = client.chat.completions.create(
+     #      model="meta-llama/Llama-3-70b-chat-hf",
+     #      messages=[{"role": "system", "content": systemPrompt}, {"role": "user", "content": userPrompt4}]
+     # )
+
      answer1 = response1.choices[0].message.content
-     # answer2 = response2.choices[0].message.content
-     # answer3 = response3.choices[0].message.content
-     answer4 = response4.choices[0].message.content
+     answer2 = response2.choices[0].message.content
+     answer3 = response3.choices[0].message.content
+     # answer4 = response4.choices[0].message.content
      # print("Answer 2: " + answer2)
 
      answerLists = [] # I've used this along with suffixing '1' to a bunch of variäbles in case we want to test different prompts side by side
      answerLists.append(eval(answer1))
-     # answerLists.append(eval(answer2))
-     # answerLists.append(eval(answer3))
-     answerLists.append(eval(answer4))
+     answerLists.append(eval(answer2))
+     answerLists.append(eval(answer3))
+     # answerLists.append(eval(answer4))
      for num, answer in enumerate(answerLists):
           numCorrect = 0
           targetsCopy = eval(line["targets"])
